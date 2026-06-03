8ce35d5b6d381132b9715db7884b9d7ede1250a8
Murilo <23004326@uepg.br>
2026-06-03 01:50:41 -0300

    Atualizando URL da API

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:3000";
+const API = "https://meu-catalogo.onrender.com";
 
 const form = document.getElementById("formulario");
 const lista = document.getElementById("lista");
